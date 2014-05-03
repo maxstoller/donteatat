3a7e1b361831ebc4ed706a255fca06c0bfb2d8cd
maxstoller <maxstoller@gmail.com>
2014-05-03 19:16:00 -0400

    Fix multiline string bug.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   def callback
-    url = "https://foursquare.com/oauth2/access_token
-      ?client_id=#{KEYS[:foursquare][:client_id]}
-      &client_secret=#{KEYS[:foursquare][:client_secret]}
-      &grant_type=authorization_code
-      &redirect_uri=http://www.donteat.at/callback
-      &code=#{params[:code]}"
+    url = "https://foursquare.com/oauth2/access_token" +
+      "?client_id=#{KEYS[:foursquare][:client_id]}" +
+      "&client_secret=#{KEYS[:foursquare][:client_secret]}" +
+      "&grant_type=authorization_code" +
+      "&redirect_uri=http://www.donteat.at/callback" +
+      "&code=#{params[:code]}"
     response = HTTParty.get(url).parsed_response
 
     User.create_from_access_token(response['access_token'])
