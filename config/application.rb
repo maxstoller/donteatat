33c58564c919c42b2efdbc932e33915a8df70c03
maxstoller <maxstoller@gmail.com>
2014-05-03 16:58:10 -0400

    Serve static assets to appease Heroku.

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module Donteatat
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # https://devcenter.heroku.com/articles/rails-4-asset-pipeline
+    config.serve_static_assets = true
   end
 end
