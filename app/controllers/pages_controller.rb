a8e429bdc8e66f7a6d5ab9d2c02240be263656f5
maxstoller <maxstoller@gmail.com>
2014-05-03 19:08:25 -0400

    Append www to donteat.at URLs.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
       ?client_id=#{KEYS[:foursquare][:client_id]}
       &client_secret=#{KEYS[:foursquare][:client_secret]}
       &grant_type=authorization_code
-      &redirect_uri=http://donteat.at/callback
+      &redirect_uri=http://www.donteat.at/callback
       &code=#{params[:code]}"
     response = HTTParty.get(url).parsed_response
 
